865cb4fee8a7cf706cc9d3f451ae81bb26a69710
yangmengwei <1635433207@qq.com>
2020-03-17 20:23:09 +0800

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="hello">
+    <!-- {{money}} -->
+    <!-- <Son1 :money="money"></Son1> -->
+    <!-- //子组件通信父组件$emit使用 -->
+    父组件：{{money}}
+    <Son1 :money="money" @input="change"></Son1>
+  </div>
+</template>
+
+<script>
+import Son1 from '@/components/Son1'
+export default {
+           methods: {
+             change(data){
+               this.money=data
+             }
+           },
+           components:{
+            Son1
+        },
+         data(){
+          return {money:100}
+        },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ src/components/Son1.vue
@@ -0,0 +1,35 @@
+<!-- z子组件 -->
+<template>
+    <div>
+        子组件1:{{money}}
+        <button @click="$emit('input',200)">修改父组件的值</button><Son1></Son1>
+    </div>
+</template>
+
+<script>
+export default {
+  props:{
+      value:{
+          type:Number,
+          default:1
+      }
+    },
+    //如果是数组
+    // props:{
+    //   value:{
+    //     type:Array,
+    //     default:()=>[]
+    //   }
+    // },
+  // data () {
+  //   return {
+
+  //   };
+    
+  // },
+}
+
+</script>
+<style  scoped>
+
+</style>
